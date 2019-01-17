88ebbcec7c1a0247c14ed011c47ec23ea4903055
Will Piears <willpiears@live.co.uk>
2019-01-17 20:33:09 +0000

    Initial commit

---
+++ src/main/java/com/willpiears/assignment1spring/Assignment1SpringApplication.java
@@ -0,0 +1,14 @@
+package com.willpiears.assignment1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1SpringApplication.class, args);
+	}
+
+}
+
