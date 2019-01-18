b7ed7c4de69d1c6ae5c9b3e5b785500e2b3ff4e5
Will Piears <willpiears@live.co.uk>
2019-01-18 17:23:51 +0000

    Add thymeleaf, bootstrap and view for dvds

--- src/main/java/com/willpiears/assignment1spring/controller/DvdsController.java
+++ src/main/java/com/willpiears/assignment1spring/controller/DvdsController.java
@@ -1,27 +1,36 @@
 package com.willpiears.assignment1spring.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.willpiears.assignment1spring.domain.DvdEntry;
 import com.willpiears.assignment1spring.service.DvdService;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class DvdsController
 {
     @Autowired
     private DvdService dvdService;
 
+    private static final String DVD_TEMPLATE = "dvds";
+
     @GetMapping ("/")
-    public String testMapping () {
-        return "Hello, World";
+    public String displayDvds (Model model) {
+        model.addAttribute ("entries", this.dvdService.findAllEntries());
+        model.addAttribute ("newEntry", new DvdEntry());
+
+        return DVD_TEMPLATE;
     }
 
+    @ResponseBody
     @GetMapping ("/dvds")
     public List<DvdEntry> retrieveAllDvds () {
         return dvdService.findAllEntries();
     }
 
+    @ResponseBody
     @GetMapping ("/dvds/{id}")
     public DvdEntry retrieveDvdEntryById (@PathVariable ("id") Integer id) {
         return this.dvdService.findDvdEntryById(id);
