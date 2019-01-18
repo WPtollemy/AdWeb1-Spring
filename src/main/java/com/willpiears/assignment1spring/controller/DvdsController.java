60b442facdaa77ec6e245b7fd768b3368a2e7fa6
Will Piears <willpiears@live.co.uk>
2019-01-18 11:47:23 +0000

    Add retrieve route

--- src/main/java/com/willpiears/assignment1spring/controller/DvdsController.java
+++ src/main/java/com/willpiears/assignment1spring/controller/DvdsController.java
@@ -3,12 +3,22 @@ package com.willpiears.assignment1spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import com.willpiears.assignment1spring.domain.DvdEntry;
+import com.willpiears.assignment1spring.service.DvdService;
 
 @RestController
 public class DvdsController
 {
+    @Autowired
+    private DvdService dvdService;
+
     @GetMapping ("/")
     public String testMapping () {
         return "Hello, World";
+    }
+
+    @GetMapping ("/dvds")
+    public List<DvdEntry> retrieveMapping () {
+        return dvdService.findAllEntries();
     }
 }
