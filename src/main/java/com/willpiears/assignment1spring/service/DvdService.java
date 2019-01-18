1564820a24ff6d14f1ec7b3b51930eaba9ece5d8
Will Piears <willpiears@live.co.uk>
2019-01-18 11:47:36 +0000

    Add service layer and retrieve all dvds

---
+++ src/main/java/com/willpiears/assignment1spring/service/DvdService.java
@@ -0,0 +1,21 @@
+package com.willpiears.assignment1spring.service;
+
+import com.willpiears.assignment1spring.domain.DvdEntry;
+import com.willpiears.assignment1spring.domain.DvdEntryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DvdService
+{
+    @Autowired
+    private DvdEntryRepository dvdEntryRepository;
+
+    public List<DvdEntry> findAllEntries()
+    {
+        return this.dvdEntryRepository.findAll();
+    }
+}
